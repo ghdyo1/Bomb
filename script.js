4efba79f98cf2c997324da7a5b4c39cf3cd26994
ghdyo1 <134512477+ghdyo1@users.noreply.github.com>
2024-04-29 00:26:58 +0300

    рандомы

---
+++ script.js
@@ -0,0 +1,20 @@
+let wiresStatus = document.querySelector("#wires .status");
+let keypadStatus = document.querySelector("#keypad .status");
+let buttonStatus = document.querySelector("#button .status");
+let simpletonStatus = document.querySelector("#simpleton .status");
+let finStatus = document.querySelector("#forget-it-not .status");
+
+let wiresRandom1 = (Math.floor((Math.random()) * 10)) % 5;
+let wiresRandom2 = (Math.floor((Math.random()) * 10)) % 5;
+let wiresRandom3 = (Math.floor((Math.random()) * 10)) % 5;
+let wiresRandom4 = (Math.floor((Math.random()) * 10)) % 5;
+
+let buttonRandom = (Math.floor((Math.random()) * 10)) % 4;
+let buttonStripRandom = (Math.floor((Math.random()) * 10)) % 4;
+
+let keypadRandom1 = Math.floor((Math.random()) * 10);
+let keypadRandom2 = Math.floor((Math.random()) * 10);
+let keypadRandom3 = Math.floor((Math.random()) * 10);
+let keypadRandom4 = Math.floor((Math.random()) * 10);
+
+let finRandom = Math.floor((Math.random()) * 10);
