45e868d0bf1165e05856e9ef19f023118f6c5fca
ghdyo1 <134512477+ghdyo1@users.noreply.github.com>
2024-04-29 14:08:26 +0300

    wire colors

--- script.js
+++ script.js
@@ -4,17 +4,47 @@ let buttonStatus = document.querySelector("#button .status");
 let simpletonStatus = document.querySelector("#simpleton .status");
 let finStatus = document.querySelector("#forget-it-not .status");
 
-let wiresRandom1 = (Math.floor((Math.random()) * 10)) % 5;
-let wiresRandom2 = (Math.floor((Math.random()) * 10)) % 5;
-let wiresRandom3 = (Math.floor((Math.random()) * 10)) % 5;
-let wiresRandom4 = (Math.floor((Math.random()) * 10)) % 5;
+let wiresRandom = (Math.floor((Math.random()) * 10)) % 5;
 
 let buttonRandom = (Math.floor((Math.random()) * 10)) % 4;
 let buttonStripRandom = (Math.floor((Math.random()) * 10)) % 4;
 
-let keypadRandom1 = Math.floor((Math.random()) * 10);
-let keypadRandom2 = Math.floor((Math.random()) * 10);
-let keypadRandom3 = Math.floor((Math.random()) * 10);
-let keypadRandom4 = Math.floor((Math.random()) * 10);
+let keypadRandom = Math.floor((Math.random()) * 10);
 
 let finRandom = Math.floor((Math.random()) * 10);
+
+let wire1 = document.querySelector(".wire1");
+let wire2 = document.querySelector(".wire2");
+let wire3 = document.querySelector(".wire3");
+let wire4 = document.querySelector(".wire4");
+
+function wiresColors(){
+    if(wiresRandom == 0){
+        wiresRandom = "#ff0000";
+    }
+    if(wiresRandom == 1){
+        wiresRandom = "#ffff00";
+    }
+    if(wiresRandom == 2){
+        wiresRandom = "#0000ff";
+    }
+    if(wiresRandom == 3){
+        wiresRandom = "#000000";
+    }
+    if(wiresRandom == 4){
+        wiresRandom = "#ffffff";
+    }
+}
+
+wiresColors()
+wire1.style.backgroundColor = wiresRandom;
+wiresRandom = (Math.floor((Math.random()) * 10)) % 5;
+wiresColors()
+wire2.style.backgroundColor = wiresRandom;
+wiresRandom = (Math.floor((Math.random()) * 10)) % 5;
+wiresColors()
+wire3.style.backgroundColor = wiresRandom;
+wiresRandom = (Math.floor((Math.random()) * 10)) % 5;
+wiresColors()
+wire4.style.backgroundColor = wiresRandom;
+wiresRandom = (Math.floor((Math.random()) * 10)) % 5;
