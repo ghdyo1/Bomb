26e4720704dd242087f223406b54e04cbf0ae597
ghdyo1 <134512477+ghdyo1@users.noreply.github.com>
2024-07-30 21:17:43 +0300

    New js later

---
+++ script-new.js
@@ -0,0 +1,46 @@
+let bomb = document.querySelector(".bomb");
+
+let wiresStatus = document.querySelector("#wires .status");
+let keypadStatus = document.querySelector("#keypad .status");
+let buttonStatus = document.querySelector("#button .status");
+let souvenirStatus = document.querySelector("#souvenir .status");
+let finStatus = document.querySelector("#forget-it-not .status");
+let timerStatus = document.querySelector(".timer .status");
+
+let wire1 = document.querySelector(".wire1");
+let wire2 = document.querySelector(".wire2");
+let wire3 = document.querySelector(".wire3");
+let wire4 = document.querySelector(".wire4");
+
+let key1 = document.querySelector(".key1");
+let key2 = document.querySelector(".key2");
+let key3 = document.querySelector(".key3");
+let key4 = document.querySelector(".key4");
+
+let button = document.querySelector(".button");
+let buttonText = document.querySelector(".button-text");
+let strip = document.querySelector(".strip");
+
+let finKey1 = document.querySelector(".fin1");
+let finKey2 = document.querySelector(".fin2");
+let finKey3 = document.querySelector(".fin3");
+let finKey4 = document.querySelector(".fin4");
+let finKey5 = document.querySelector(".fin5");
+let finKey6 = document.querySelector(".fin6");
+let finKey7 = document.querySelector(".fin7");
+let finKey8 = document.querySelector(".fin8");
+let finKey9 = document.querySelector(".fin9");
+let finKey0 = document.querySelector(".fin0");
+let stageHTML = document.querySelector(".stage");
+let finDigit = document.querySelector(".number");
+
+let question = document.querySelector(".question");
+let answer1 = document.querySelector(".answer1");
+let answer2 = document.querySelector(".answer2");
+let answer3 = document.querySelector(".answer3");
+let answer4 = document.querySelector(".answer4");
+let answer5 = document.querySelector(".answer5");
+
+let strike1 = document.querySelector(".strike1");
+let strike2 = document.querySelector(".strike2");
+
